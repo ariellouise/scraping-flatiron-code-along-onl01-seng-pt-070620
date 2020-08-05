7df1f560140d5c2fce03493c8660f2708a4cee3c
ariel murphy <arielladders@gmail.com>
2020-08-05 03:03:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,3 +13,19 @@ end
 
 Scraper.new.get_page
   
+  
+  
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+end
+
+
+
