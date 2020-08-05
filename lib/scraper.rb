109cc1f9a80460d5a739b61e1aa4a27f890e7983
ariel murphy <arielladders@gmail.com>
2020-08-05 03:05:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,17 +10,7 @@ class Scraper
   end 
 
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end 
-  end  
-end 
+
 
  def get_courses
     self.get_page.css(".post")
@@ -33,4 +23,5 @@ end
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
-
+  end 
+end 
